8f99f75111afcad39753877dbbb2635f6f599198
EmJhey <emjheypb@gmail.com>
2023-08-25 15:33:11 -0400

    render arrays from api data

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -2,37 +2,86 @@ import React from "react";
 
 export default class FetchRandomUser extends React.Component {
   state = {
-    person: null,
+    people: [],
+    loading: true,
   };
 
   // componentDidMount knows if component has rendered at least once
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    const url = "https://api.randomuser.me/?results=5";
     // use await for async
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0] });
-    console.log(this.state.person);
+    this.setState({ people: data.results, loading: false });
+    console.log(this.state.people);
   }
 
   render() {
+    if (this.state.loading) {
+      return <div>L O A D I N G</div>;
+    }
+
+    if (!this.state.people.length) {
+      return <div>NO DATA</div>;
+    }
+
+    // #region OPTION 1
     return (
       <div>
-        {!this.state.person ? (
-          <div>LOADING</div>
-        ) : (
-          <div>
+        {this.state.people.map((person, index) => (
+          <div key={person.login.uuid}>
             <div>
-              <img src={this.state.person.picture.large} alt="random person" />
+              <img src={person.picture.large} alt="random person" />
             </div>
-            {this.state.person.name.title +
+            {index +
+              1 +
+              " : " +
+              person.name.title +
               " " +
-              this.state.person.name.first +
+              person.name.first +
               " " +
-              this.state.person.name.last}
+              person.name.last}
           </div>
-        )}
+        ))}
       </div>
     );
+    // #endregion OPTION 1
+
+    // #region OPTION 2 Variable
+    // const peopleJSX = this.state.people.map((person, index) => (
+    //   <div key={person.login.uuid}>
+    //     {index +
+    //       1 +
+    //       " : " +
+    //       person.name.title +
+    //       " " +
+    //       person.name.first +
+    //       " " +
+    //       person.name.last}
+    //     <div>
+    //       <img src={person.picture.large} alt="random person" />
+    //     </div>
+    //   </div>
+    // ));
+
+    // return <div>{peopleJSX}</div>;
+    // #endregion OPTION 2 Variable
+
+    // #region OPTION 3 For Loop
+    // const peopleJSX = [];
+
+    // this.state.people.forEach((person) => {
+    //   peopleJSX.push(
+    //     <div key={person.login.uuid}>
+    //       {person.name.title + " " + person.name.first + " " + person.name.last}
+    //       <div>
+    //         <img src={person.picture.large} alt="random person" />
+    //       </div>
+    //     </div>
+    //   );
+    // });
+
+    // return <div>{peopleJSX}</div>;
+    // #endregion OPTION 3 For Loop
   }
 }
