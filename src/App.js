f9f46386b12489ca9ec62195b60d92c6d163fcc9
EmJhey <emjheypb@gmail.com>
2023-08-25 15:50:31 -0400

    counter with clones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import "./App.css";
-import FetchRandomUser from "./components/FetchRandomUser";
+import Counter from "./components/Counter";
 
 class App extends Component {
   state = {
@@ -13,10 +13,43 @@ class App extends Component {
     });
   };
 
+  state = {
+    count: 0,
+  };
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <FetchRandomUser />
+        <Counter
+          count={this.state.count}
+          increment={this.incrementCount}
+          decrement={this.decrementCount}
+          team="Original"
+        />
+        <Counter
+          count={this.state.count}
+          increment={this.incrementCount}
+          decrement={this.decrementCount}
+          team="Clone 1"
+        />
+        <Counter
+          count={this.state.count}
+          increment={this.incrementCount}
+          decrement={this.decrementCount}
+          team="Clone 2"
+        />
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,44 +1,14 @@
 import React from "react";
 
 export default class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: props.initialCount,
-    };
-  }
-
-  // react special function
-  // needs to be a class component
-  componentWillUnmount() {
-    console.log("Unmounting...");
-  }
-
-  // react special function
-  // needs to be a class component
-  componentDidMount() {
-    console.log("Mounting...");
-  }
-
-  incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  decrementCount = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
-
   render() {
     return (
       <div>
-        <div>count: {this.state.count} </div>
-        <button onClick={this.decrementCount}>- 1</button>
-        <button onClick={this.incrementCount}>+ 1</button>
+        {this.props.team}
+        <div>
+          <button onClick={this.props.decrement}>- 1</button> {this.props.count}{" "}
+          <button onClick={this.props.increment}>+ 1</button>
+        </div>
       </div>
     );
   }
